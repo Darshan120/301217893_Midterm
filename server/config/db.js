d9e779b7b33f446f9544bbd57ab15914ef961391
Darshan120 <90949001+Darshan120@users.noreply.github.com>
2021-11-06 20:11:58 -0400

    Midterm

---
+++ server/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Darshan:Kedr4LnZrcFR67aN@mongodatabaseservermidt.iovcb.mongodb.net/books?retryWrites=true&w=majority"
+};
